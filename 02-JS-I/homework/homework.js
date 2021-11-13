6877998936d9ef70de07b9edf9829e6e8767d221
unknown <miguel.barrios@aerolaplata.com.ar>
2021-11-12 21:01:41 -0300

    homework02

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -239,13 +239,13 @@ function esVocal(letra){
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
   if(letra.length >1) {
-    return 'Dato incorrecto';
+    return "Dato incorrecto";
   }
-  if(letra in ('a','e','i','o','u')) {
-    return 'Es vocal';
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+    return "Es vocal";
   }
   else {
-    return 'Dato incorrecto';
+    return "Dato incorrecto";
   }      
 }
 
